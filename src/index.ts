b59fe91f1725c6fcb4f7aea98baa623ac7fbcb2c
Richard Olsson <r@richardolsson.se>
2021-03-27 14:12:14 +0100

    Add simple proxying

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-export default function moxy() {
-    return null;
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { Server } from 'node:http';
+
+interface MoxyConfig {
+    port?: number,
+    forward?: string;
+};
+
+export default function moxy(config? : MoxyConfig) {
+    const forward = config?.forward;
+    const port = config?.port ?? 8001;
+
+    const app = express();
+
+    if (forward) {
+        app.use(createProxyMiddleware({
+            target: forward,
+            changeOrigin: true,
+        }));
+    }
+
+    let server : Server;
+
+    return {
+        start: () => {
+            server = app.listen(port);
+            return server;
+        },
+
+        stop: () => {
+            server.close();
+        }
+    };
 }
