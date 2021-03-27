6529cf50b369e564690f107c1ffc65729d2108bf
Richard Olsson <r@richardolsson.se>
2021-03-27 15:33:06 +0100

    Add API to delete mocks

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,9 @@ interface Mock {
     }
 };
 
+const RES_MOCK_REGEX = /(\/.+)\/_mocks\/([a-z]+)$/;
+const RES_MOCKS_REGEX = /(\/.+)\/_mocks$/;
+
 export default function moxy(config? : MoxyConfig) {
     const forward = config?.forward;
     const port = config?.port ?? 8001;
@@ -26,14 +29,14 @@ export default function moxy(config? : MoxyConfig) {
 
     app.use(bodyParser.json());
 
-    const mocks : Mock[] = [];
+    let mocks : Mock[] = [];
 
     function findMock(path : string, method : string) : Mock | null {
         const mock = mocks.find(m => m.path === path && m.method === method.toLowerCase());
         return mock || null;
     }
 
-    app.put(/(\/.+)\/_mocks\/([a-z]+)$/, (req, res) => {
+    app.put(RES_MOCK_REGEX, (req, res) => {
         const mock : Mock = {
             method: req.params[1].toLowerCase(),
             path: req.params[0],
@@ -52,6 +55,24 @@ export default function moxy(config? : MoxyConfig) {
             mocks.push(mock);
             res.status(201).end();
         }
+    });
+
+    // Delete single path-method mock
+    app.delete(RES_MOCK_REGEX, (req, res) => {
+        const existing = findMock(req.params[0], req.params[1]);
+        if (existing) {
+            mocks = mocks.filter(m => m !== existing);
+            res.status(204).end();
+        }
+        else {
+            res.status(404).end();
+        }
+    });
+
+    // Delete all mocks from a path
+    app.delete(RES_MOCKS_REGEX, (req, res) => {
+        mocks = mocks.filter(m => m.path !== req.path);
+        res.status(204).end();
     });
 
     app.use((req, res, next) => {
