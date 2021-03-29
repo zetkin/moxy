9cfc54927a6adbb18b3a21b2f8228c6b524afd23
Richard Olsson <r@richardolsson.se>
2021-03-29 12:12:54 +0200

    Add CLI to start proxy

---
+++ src/cli.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import commandLineArgs from 'command-line-args';
+import moxy from './';
+
+const options = commandLineArgs([
+    { name: 'port', alias: 'p', type: Number },
+    { name: 'forward', alias: 'f', type: String },
+]);
+
+const proxy = moxy({
+    port: options.port,
+    forward: options.forward,
+});
+
+proxy.start();
